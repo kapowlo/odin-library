73656b7ed710004b9f969918545a8417df6f83cd
mano <rsmike91@outlook.com>
2024-02-29 23:40:48 -0500

    Added if conditional test to btnAddNewBooks event had a problem with multiple forms popping up when add new book button was clicked now with the conditional test if the form exists the if test fails and form wont be created again otherwise if querySelector returns null which is falsy, the conditional test will pass and the form will be created a long with all the content that needs to be inside of it there should never be more than 1 form on the page

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function displayCardBook(arr){
 }
 
 
-/*assignment 4 todo:make a reset button
+/*assignment 6 todo:Add a button on each book’s display to change its read status
 */
 const btnAddNewBooks = document.querySelector("#add-book");
 console.log(btnAddNewBooks);
@@ -77,132 +77,135 @@ btnAddNewBooks.addEventListener("click",(e)=>{
     //creates a new book object then calls addBookToLibrary then displayCardBook
     //once everything is done the form is hidden
     
-    const myForm=document.createElement("form")
-    myForm.classList.add("my-form")
-    bookSection.insertBefore(myForm,bookSection.children[2]); // put the form right after the button
-
-    //create four input fields with labels and set their attributes
-    const inputTitle=document.createElement("input")
-    const labelTitle=document.createElement("label")
-    inputTitle.setAttribute("type","text");
-    inputTitle.setAttribute("id","title");
-    labelTitle.textContent="Title:";
-    labelTitle.setAttribute("for","title");
-
-    const inputAuthor=document.createElement("input")
-    const labelAuthor=document.createElement("label")
-    inputAuthor.setAttribute("type","text");
-    inputAuthor.setAttribute("id","author");
-    labelAuthor.textContent="Author:";
-    labelAuthor.setAttribute("for","author");
-
-
-    const inputPage=document.createElement("input")
-    const labelPage=document.createElement("label")
-    inputPage.setAttribute("type","text");
-    inputPage.setAttribute("id","page");
-    labelPage.textContent="Page:";
-    labelPage.setAttribute("for","page");
    
-    const inputRead=document.createElement("input")
-    const labelRead=document.createElement("label")
-    inputRead.setAttribute("type","text");
-    inputRead.setAttribute("id","read");
-    labelRead.textContent="Read:";
-    labelRead.setAttribute("for","read");
-   
-    //make 4 div wrappers around each pair of input label append them to my form
-    const wrapper1=document.createElement("div")
-    wrapper1.classList.add("wrapper-1");
+    //conditional test if form already exists don't recreate it,don't execute the if block
+    //otherwise if querySelector returns null which is falsy it means the form doesn't exist 
+    //and the if block is true. So the conditional test will run
+    // it will create the form a long with all content that needs to be inside the form
+    if(!document.querySelector(".my-form")){ 
+        const myForm=document.createElement("form")
+        myForm.classList.add("my-form")
+        bookSection.insertBefore(myForm,bookSection.children[2]); // put the form right after the button
+        //create four input fields with labels and set their attributes
+        const inputTitle=document.createElement("input")
+        const labelTitle=document.createElement("label")
+        inputTitle.setAttribute("type","text");
+        inputTitle.setAttribute("id","title");
+        labelTitle.textContent="Title:";
+        labelTitle.setAttribute("for","title");
 
-    const wrapper2=document.createElement("div")
-    wrapper2.classList.add("wrapper-2");
-
-    const wrapper3=document.createElement("div")
-    wrapper3.classList.add("wrapper-3");
-
-    const wrapper4=document.createElement("div")
-    wrapper4.classList.add("wrapper-4");
-    myForm.append(wrapper4);
-   
-    // append all div wrapper to my form
-    myForm.append(wrapper1,wrapper2,wrapper3,wrapper4); 
-
-    // append all pairs of inputs/label to each div wrappers
-    wrapper1.append(labelTitle,inputTitle);
-    wrapper2.append(labelAuthor,inputAuthor);
-    wrapper3.append(labelPage,inputPage);
-    wrapper4.append(labelRead,inputRead);
+        const inputAuthor=document.createElement("input")
+        const labelAuthor=document.createElement("label")
+        inputAuthor.setAttribute("type","text");
+        inputAuthor.setAttribute("id","author");
+        labelAuthor.textContent="Author:";
+        labelAuthor.setAttribute("for","author");
 
 
-    //create submit btn add class 
-    const submitBtn =document.createElement("button");
-    submitBtn.classList.add("btn-submit");
-    submitBtn.textContent="Submit";
+        const inputPage=document.createElement("input")
+        const labelPage=document.createElement("label")
+        inputPage.setAttribute("type","text");
+        inputPage.setAttribute("id","page");
+        labelPage.textContent="Page:";
+        labelPage.setAttribute("for","page");
     
+        const inputRead=document.createElement("input")
+        const labelRead=document.createElement("label")
+        inputRead.setAttribute("type","text");
+        inputRead.setAttribute("id","read");
+        labelRead.textContent="Read:";
+        labelRead.setAttribute("for","read");
+    
+        //make 4 div wrappers around each pair of input label append them to my form
+        const wrapper1=document.createElement("div")
+        wrapper1.classList.add("wrapper-1");
 
-    // create reset button add class
-    const resetBtn = document.createElement("button");
-    resetBtn.classList.add("btn-reset");
-    resetBtn.textContent="Reset"
+        const wrapper2=document.createElement("div")
+        wrapper2.classList.add("wrapper-2");
 
-    // create wrapper for buttons 
-    const formBtnWrapper=document.createElement("div");
-    formBtnWrapper.classList.add("form-btn-wrapper")
+        const wrapper3=document.createElement("div")
+        wrapper3.classList.add("wrapper-3");
 
-    //append both buttons two wrapper
-    formBtnWrapper.append(submitBtn,resetBtn);
+        const wrapper4=document.createElement("div")
+        wrapper4.classList.add("wrapper-4");
+        myForm.append(wrapper4);
+    
+        // append all div wrapper to my form
+        myForm.append(wrapper1,wrapper2,wrapper3,wrapper4); 
 
-    // append wrapper div to form
-    myForm.append(formBtnWrapper);
-
-    //add event for submit button and reset button both e.preventDefault()
-    // method on submit btn to stop default behavior of submitting form data 
-    // reset button all input field values should be reset to empty string
-
-    submitBtn.addEventListener("click",(e)=>{
-        e.preventDefault()// prevents form from submitting data
-        // retrieve the values of the input fields
-       let inputVal1=document.getElementById('title').value
-       let inputVal2=document.getElementById('author').value
-       let inputVal3=document.getElementById('page').value
-       let inputVal4=document.getElementById('read').value
-        console.log(inputVal1,inputVal2,inputVal3,inputVal4);
-        // make new book object from Book constructor pass each input values as argument 
-        const book=new Book(inputVal1,inputVal2,inputVal3,inputVal4);
-       
-        // HERE add lines that calls the method removeBooks()
-        book.removeBooks();
-        // add this book to library array by calling function,each book is an object
-        console.log(addBookToLibrary(book));
-
-        //display this as book card by calling function displayCardBook and passing library
-        console.log(displayCardBook(myLibrary));
-
-        //removes the form element from the dom better than hiding it...since I am re-creating the form every time I click add new book
-        myForm.remove();
-
-        // test to see if i can loop and what it displays  keep this for now
-        for(let i=0;i<myLibrary.length;i++){
-            console.log("looping through library",(myLibrary[i]));
-        }
+        // append all pairs of inputs/label to each div wrappers
+        wrapper1.append(labelTitle,inputTitle);
+        wrapper2.append(labelAuthor,inputAuthor);
+        wrapper3.append(labelPage,inputPage);
+        wrapper4.append(labelRead,inputRead);
 
 
-         //solves bug where the same book would appear twice
-         console.log("popping the book from my library....to avoid duplicate showing up",);
-         myLibrary.pop();
-         
-    })
-    //when reset button is pressed it removes any value entered inside all input fields
-    resetBtn.addEventListener("click",(e)=>{
-        e.preventDefault();//prevent button from submitting form data
-        console.log("Reset button was clicked removing all content from input fields")
-        inputTitle.value="";
-        inputAuthor.value="";
-        inputPage.value="";
-        inputRead.value="";
-    })
+        //create submit btn add class 
+        const submitBtn =document.createElement("button");
+        submitBtn.classList.add("btn-submit");
+        submitBtn.textContent="Submit";
+        
 
-    // display elements inside of form in console
-    console.log(myForm);
+        // create reset button add class
+        const resetBtn = document.createElement("button");
+        resetBtn.classList.add("btn-reset");
+        resetBtn.textContent="Reset"
+
+        // create wrapper for buttons 
+        const formBtnWrapper=document.createElement("div");
+        formBtnWrapper.classList.add("form-btn-wrapper")
+
+        //append both buttons two wrapper
+        formBtnWrapper.append(submitBtn,resetBtn);
+
+        // append wrapper div to form
+        myForm.append(formBtnWrapper);
+
+        //add event for submit button and reset button both e.preventDefault()
+        // method on submit btn to stop default behavior of submitting form data 
+        // reset button all input field values should be reset to empty string
+
+        submitBtn.addEventListener("click",(e)=>{
+            e.preventDefault()// prevents form from submitting data
+            // retrieve the values of the input fields
+        let inputVal1=document.getElementById('title').value
+        let inputVal2=document.getElementById('author').value
+        let inputVal3=document.getElementById('page').value
+        let inputVal4=document.getElementById('read').value
+            console.log(inputVal1,inputVal2,inputVal3,inputVal4);
+            // make new book object from Book constructor pass each input values as argument 
+            const book=new Book(inputVal1,inputVal2,inputVal3,inputVal4);
+        
+            // HERE add lines that calls the method removeBooks()
+            book.removeBooks();
+            // add this book to library array by calling function,each book is an object
+            console.log(addBookToLibrary(book));
+
+            //display this as book card by calling function displayCardBook and passing library
+            console.log(displayCardBook(myLibrary));
+
+            //removes the form element from the dom better than hiding it...since I am re-creating the form every time I click add new book
+            myForm.remove();
+
+            // test to see if i can loop and what it displays  keep this for now
+            for(let i=0;i<myLibrary.length;i++){
+                console.log("looping through library",(myLibrary[i]));
+            }
+
+
+            //solves bug where the same book would appear twice
+            console.log("popping the book from my library....to avoid duplicate showing up",);
+            myLibrary.pop();
+            
+        })
+        //when reset button is pressed it removes any value entered inside all input fields
+        resetBtn.addEventListener("click",(e)=>{
+            e.preventDefault();//prevent button from submitting form data
+            console.log("Reset button was clicked removing all content from input fields")
+            inputTitle.value="";
+            inputAuthor.value="";
+            inputPage.value="";
+            inputRead.value="";
+        })
+    }
 });
